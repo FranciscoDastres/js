ed4e10cebdca08e02707131bef7f2882989e627e
Francisco <franciscomeza.1234@gmail.com>
2023-01-09 22:51:58 -0300

    ejercios variados

---
+++ ejercicio.js
@@ -0,0 +1,24 @@
+/* 4 atletas
+de cuatro corredores de atletismo se sabe que C ha llegado 
+inmediatamente detras de B y D ha llegado en medio de A y C podria usted calcular el orden de llegada?
+b adelante de c d 
+b primero segundo 
+b
+b
+c
+d
+a */
+
+var a = {
+
+    A:0,
+    B:0,
+    C:0,
+    D:0,   
+    resultado: function(){
+
+        if(a.C > a.B &&
+            a.D > a.A)
+
+    }
+}
